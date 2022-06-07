c9f43ffcceb1327a1d088ed039de2918e159861e
Matheus Paim <matheus.paiim@gmail.com>
2022-06-07 19:58:38 -0300

    tip

--- tip/tip.py
+++ tip/tip.py
@@ -1,14 +1,16 @@
 def main():
-    dollars = dollars_to_float(input("How much was the meal? ").strip('$'))
-    percent = percent_to_float(input("What percentage would you like to tip? ").strip('%'))
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-def dollars_to_float(dollars):
-    dollars
-    return float()
+def dollars_to_float(d):
+    doll = d.strip('$')
+    return float(doll)
 
-def percent_to_float(percent):
-    return float(percent)
+def percent_to_float(p):
+    perc = p.strip('%')
+    perc_divide = float(perc) / 100
+    return perc_divide
 
 main()
