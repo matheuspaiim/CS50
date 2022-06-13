adb3a70e19d3ca97c70d3eda94834f3f39bff977
Matheus Paim <matheus.paiim@gmail.com>
2022-06-13 16:20:49 -0300

    coke

--- coke/coke.py
+++ coke/coke.py
@@ -1,34 +1,45 @@
 # :) coke.py exists
-# :( coke accepts 25 cents
-   # expected "25", not ""
-# :( coke accepts 10 cents
-   # expected "40", not ""
-# :( coke accepts 5 cents
-   # expected "45", not ""
-# :( coke rejects invalid amount of cents
-   # expected "50", not ""
-# :( coke accepts continued input
-   # expected prompt for input, found none
-# :( coke terminates at 50 cents
-   # expected prompt for input, found none
-# :( coke provides correct change
-   # expected prompt for input, found none
+# :) coke accepts 25 cents
+# :) coke accepts 10 cents
+# :) coke accepts 5 cents
+# :) coke rejects invalid amount of cents
+# :) coke accepts continued input
+# :) coke terminates at 50 cents
+# :) coke provides correct change
 
 def main():
+
     global amount
     amount = 50
-    print(f"Amount Due: {amount}")
     coke()
     ...
 
 def coke():
-    global coin
-    buy = amount - coin
 
-    while amount <= 50:
+    global amount
+    print(f"Amount Due: {amount}")
+
+    while amount <= 50 and amount >= 0:
+
         coin = int(input("Insert coin: "))
-        if coin == 25 or coin == 10 or coin == 5:
+        buy = amount - coin
+        change = abs(buy)
+
+        if buy > 0 and coin in [25, 10, 5]:
             print(f"Amount Due: {buy}")
+            amount -= coin
+
+        elif buy == 0:
+            print(f"Change Owed: {buy}")
+            break
+
+        elif buy < 0:
+            print(f"Change Owed: {change}")
+            break
+
+        else:
+            print(f"Amount Due: {amount}")
+
     ...
 
 if __name__ == "__main__":
